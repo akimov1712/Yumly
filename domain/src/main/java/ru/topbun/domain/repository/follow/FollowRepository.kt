7c18f38ca2844570297696d5b6642ba1ee7f0eac
akimov1712 <akimov1712@mail.ru>
2025-10-07 14:43:06 +0300

    feat: impl domain layer for get following

--- domain/src/main/java/ru/topbun/domain/repository/follow/FollowRepository.kt
+++ domain/src/main/java/ru/topbun/domain/repository/follow/FollowRepository.kt
@@ -8,5 +8,6 @@ interface FollowRepository {
 
     suspend fun switchFollowUser(userId: Int): Result<Boolean, DataError>
     suspend fun getFollowers(userId: Int, limit: Int, offset: Int): Result<List<ProfileEntity>, DataError>
+    suspend fun getFollowing(userId: Int, limit: Int, offset: Int): Result<List<ProfileEntity>, DataError>
 
 }
