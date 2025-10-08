f364212e5331b303e4201f911dbdcd10c960e8ce
akimov1712 <akimov1712@mail.ru>
2025-10-08 21:47:49 +0300

    feat: impl domain layer for send message

--- domain/src/main/java/ru/topbun/domain/useCases/gpt/GetGptChatsUseCase.kt
+++ domain/src/main/java/ru/topbun/domain/useCases/gpt/GetGptChatsUseCase.kt
@@ -2,7 +2,7 @@ package ru.topbun.domain.useCases.gpt
 
 import ru.topbun.domain.repository.gpt.GptRepository
 
-class GetChatsUseCase(private val repository: GptRepository) {
+class GetGptChatsUseCase(private val repository: GptRepository) {
 
     suspend operator fun invoke(limit: Int = 20, offset: Int = 0) = repository.getChats(limit, offset)
 
