923ade923bd6bb77e0fc680669e94f298af7ed3d
akimov1712 <akimov1712@mail.ru>
2025-10-09 22:48:08 +0300

    feat: create apiClient and update jdk 11 to 17

---
+++ data/src/main/java/ru/topbun/data/source/remote/ApiFactory.kt
@@ -0,0 +1,26 @@
+package ru.topbun.data.source.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.topbun.data.BuildConfig
+
+object ApiFactory {
+
+    private fun createDefaultOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(createDefaultOkHttpClient())
+        .build()
+
+
+}
