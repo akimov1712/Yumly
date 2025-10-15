12945b905faab9fe0cfa46149800a67512bde79a
akimov1712 <akimov1712@mail.ru>
2025-10-15 21:00:57 +0300

    feat: added AddRecipeValidator

---
+++ domain/src/main/java/ru/topbun/domain/validation/recipe/AddRecipeValidator.kt
@@ -0,0 +1,32 @@
+package ru.topbun.domain.validation.recipe
+
+import ru.topbun.common.Result
+import ru.topbun.common.Validator
+import ru.topbun.common.error.ValidatorError
+import ru.topbun.domain.entity.recipe.addRecipe.AddRecipeEntity
+
+class AddRecipeValidator : Validator<AddRecipeEntity> {
+
+    override fun validate(data: AddRecipeEntity): Result<Unit, ValidatorError> {
+        val error = when {
+            data.title.length > 48 -> AddRecipeValidatorError.TITLE_LENGTH
+            (data.description?.length ?: 0) > 500 -> AddRecipeValidatorError.DESCRIPTION_LENGTH
+            data.cookingTime > 14400 -> AddRecipeValidatorError.COOKING_TIME
+            data.ingredients.size !in (1..32) -> AddRecipeValidatorError.COUNT_INGREDIENTS
+            data.steps.size !in (1..32) -> AddRecipeValidatorError.COUNT_STEPS
+            data.protein > 5000 -> AddRecipeValidatorError.COUNT_PROTEIN
+            data.carb > 5000 -> AddRecipeValidatorError.COUNT_CARBS
+            data.fat > 5000 -> AddRecipeValidatorError.COUNT_FAT
+            else -> null
+        }
+        return error?.let { Result.Error(error) } ?: run { Result.Success(Unit) }
+    }
+
+}
+
+enum class AddRecipeValidatorError : ValidatorError {
+
+    TITLE_LENGTH, DESCRIPTION_LENGTH, COOKING_TIME, COUNT_INGREDIENTS, COUNT_STEPS,
+    COUNT_PROTEIN, COUNT_CARBS, COUNT_FAT
+
+}
