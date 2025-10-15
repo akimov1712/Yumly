852bc6da6f4984461ba752cec73077e311de3f50
akimov1712 <akimov1712@mail.ru>
2025-10-15 20:34:49 +0300

    feat: impl receive recipe by id for data layer

--- data/src/main/java/ru/topbun/data/source/remote/api/recipe/RecipeApi.kt
+++ data/src/main/java/ru/topbun/data/source/remote/api/recipe/RecipeApi.kt
@@ -2,7 +2,10 @@ package ru.topbun.data.source.remote.api.recipe
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import ru.topbun.data.source.remote.dto.recipe.RecipeDto
 import ru.topbun.data.source.remote.dto.recipe.getRecipe.GetRecipeResponse
 import ru.topbun.data.source.remote.dto.recipe.getRecipe.GetRecipeRequest
 
@@ -10,5 +13,8 @@ interface RecipeApi {
 
     @POST("/v1/recipe")
     suspend fun getRecipes(@Body body: GetRecipeRequest): Response<GetRecipeResponse>
+
+    @GET("/v1/recipe/{id}")
+    suspend fun getRecipeById(@Path("id") recipeId: Int): Response<RecipeDto>
 
 }
