88a14b7285c7914c0c78c5291292fb641e621eee
akimov1712 <akimov1712@mail.ru>
2025-10-28 15:28:51 +0300

    feat: impl append and receive history queries from local db

---
+++ data/src/main/java/ru/topbun/data/source/local/database/history/HistoryDao.kt
@@ -0,0 +1,14 @@
+package ru.topbun.data.source.local.database.history
+
+import androidx.room.Insert
+import androidx.room.Query
+import ru.topbun.data.source.local.database.history.dbo.HistoryDbo
+
+interface HistoryDao {
+
+    @Query("SELECT * FROM histories")
+    suspend fun getHistory(): List<HistoryDbo>
+
+    @Insert
+    suspend fun addHistory(data: HistoryDbo)
+}
