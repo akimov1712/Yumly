83188e87889961077caa9a2a67f4f7cb0df231ab
akimov1712 <akimov1712@mail.ru>
2025-10-28 14:20:21 +0300

    feat: impl receive gpt chat by id for data layer

--- data/src/main/java/ru/topbun/data/source/remote/api/gpt/GptApi.kt
+++ data/src/main/java/ru/topbun/data/source/remote/api/gpt/GptApi.kt
@@ -2,7 +2,9 @@ package ru.topbun.data.source.remote.api.gpt
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import ru.topbun.data.source.remote.dto.gpt.chat.GetGptChatsRequest
 import ru.topbun.data.source.remote.dto.gpt.chat.GetGptChatsResponse
 
@@ -10,5 +12,8 @@ internal interface GptApi {
 
     @POST("/v1/gpt")
     suspend fun getChats(@Body body: GetGptChatsRequest): Response<List<GetGptChatsResponse>>
+
+    @GET("/v1/gpt/{id}")
+    suspend fun getChatById(@Path("id") id: Int): Response<GetGptChatsResponse>
 
 }
--- data/src/main/java/ru/topbun/data/source/remote/dto/gpt/chat/GetGptChatsResponse.kt
+++ data/src/main/java/ru/topbun/data/source/remote/dto/gpt/chat/GetGptChatsResponse.kt
@@ -1,13 +1,11 @@
 package ru.topbun.data.source.remote.dto.gpt.chat
 
-import android.R.attr.text
 import ru.topbun.domain.entity.gpt.GptChatEntity
-import ru.topbun.domain.entity.gpt.GptMessageEntity
 
 internal data class GetGptChatsResponse(
     val maxLimitMessages: Int,
     val chat: GptChatDto
-){
+) {
 
     fun toEntity() = GptChatEntity(
         id = chat.id,
