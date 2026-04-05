fa4747b3427577346ae1c961b69c79174143dfd9
KhamitTop <kairzhaniskairov@gmail.com>
2026-04-05 22:01:38 +0300

    feat: impl choice limit calories for HomeFilterDialog

--- feature/home_filter/src/main/java/ru/topbun/home_filter/HomeFilterDialog.kt
+++ feature/home_filter/src/main/java/ru/topbun/home_filter/HomeFilterDialog.kt
@@ -5,17 +5,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ru.topbun.core.ui.components.BottomDialogWrapper
 import ru.topbun.core.ui.components.Height
+import ru.topbun.core.ui.theme.Colors
+import ru.topbun.core.ui.theme.Typography
+import ru.topbun.home_filter.components.CaloriesSection
 import ru.topbun.home_filter.components.CategorySection
 import ru.topbun.home_filter.components.DurationSection
-import ru.topbun.home_filter.components.Title
-
 
 @Composable
 fun HomeFilterDialog(
@@ -44,9 +45,21 @@ internal fun FilterContent() {
         CategorySection()
         Height(20.dp)
         DurationSection()
+        Height(20.dp)
+        CaloriesSection()
     }
 }
 
 
+@Composable
+private fun Title() {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = "Добавить фильтр",
+        color = Colors.MAIN_TEXT,
+        style = Typography.H2,
+        textAlign = TextAlign.Center
+    )
+}
 
 
