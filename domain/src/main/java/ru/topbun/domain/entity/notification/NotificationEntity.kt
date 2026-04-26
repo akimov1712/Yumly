8cc8fc47c92119a1629d696546f6741ebc6da0a0
akimov1712 <akimov1712@mail.ru>
2026-04-26 10:16:53 +0000

    ref(notification): switch createdAt from Date to LocalDateTime

--- domain/src/main/java/ru/topbun/domain/entity/notification/NotificationEntity.kt
+++ domain/src/main/java/ru/topbun/domain/entity/notification/NotificationEntity.kt
@@ -3,6 +3,7 @@ package ru.topbun.domain.entity.notification
 import ru.topbun.domain.entity.account.ProfileEntity
 import ru.topbun.domain.entity.account.UserEntity
 import ru.topbun.domain.entity.recipe.RecipeEntity
+import java.time.LocalDateTime
 import java.util.Date
 
 data class NotificationEntity(
@@ -10,5 +11,5 @@ data class NotificationEntity(
     val type: NotificationType,
     val initiator: ProfileEntity,
     val recipe: RecipeEntity?,
-    val createdAt: Date
+    val createdAt: LocalDateTime
 )
